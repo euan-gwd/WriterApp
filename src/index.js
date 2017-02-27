15ad4f626869868a2b0f669cb818278a6decddb7
Euan_G <euan@amwic.co.za>
2017-02-27 14:31:05 +0200

    Added Main Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Rebase from 're-base';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Container from './components/Container';
+import NewChat from './components/NewChat';
+
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDYliJuBFjLAQO_vfH0rlioWUiKrRe_ZL4",
+  authDomain: "tchatapp-586ab.firebaseapp.com",
+  databaseURL: "https://tchatapp-586ab.firebaseio.com",
+  storageBucket: "tchatapp-586ab.appspot.com",
+  messagingSenderId: "846810590536"
+})
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+				this.state= {
+					messages: []
+				}
+  }
+
+		componentWillMount() {
+			base.bindtoState('chats',{
+				context: this,
+				state: 'messages',
+				asArray: true
+			})
+		}
+
+  render() {
+    return (
+      <div style={ { paddingTop: '30px' } }>
+							<NewChat chats={ this.state.messages}/>
+							<Container />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <App />,
+  <Main />,
   document.getElementById('root')
 );
