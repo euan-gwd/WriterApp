5b0e91796bc72702326bb1d17bdd95de00c67f3c
Euan_G <euan@amwic.co.za>
2017-02-28 16:57:30 +0200

    Added react-router

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Rebase from 're-base';
+import NavBar from './components/Navbar';
+import Container from './components/Container';
+import NewChat from './components/NewChat';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDYliJuBFjLAQO_vfH0rlioWUiKrRe_ZL4",
+  authDomain: "tchatapp-586ab.firebaseapp.com",
+  databaseURL: "https://tchatapp-586ab.firebaseio.com",
+  storageBucket: "tchatapp-586ab.appspot.com",
+  messagingSenderId: "846810590536"
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: []
+    }
+  };
+
+  componentWillMount() {
+    base.bindToState('chats', {
+      context: this,
+      state: 'messages',
+      asArray: true
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <br />
+        <NewChat chats={this.state.messages} />
+        <Container />
+      </div>
+    );
+  }
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,49 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Rebase from 're-base';
-import NavBar from './components/Navbar';
-import Container from './components/Container';
-import NewChat from './components/NewChat';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
-const base = Rebase.createClass({
-  apiKey: "AIzaSyDYliJuBFjLAQO_vfH0rlioWUiKrRe_ZL4",
-  authDomain: "tchatapp-586ab.firebaseapp.com",
-  databaseURL: "https://tchatapp-586ab.firebaseio.com",
-  storageBucket: "tchatapp-586ab.appspot.com",
-  messagingSenderId: "846810590536"
-});
-
-class Main extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      messages: []
-    }
-  };
-
-  componentWillMount() {
-    base.bindToState('chats', {
-      context: this,
-      state: 'messages',
-      asArray: true
-    })
-  };
-
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <br />
-        <NewChat chats={this.state.messages} />
-        <Container />
-      </div>
-    );
-  }
-};
-
 ReactDOM.render(
-  <Main />,
+	<Router history={hashHistory}>
+		<Route path="/" component={App} />
+	</Router>,
   document.getElementById('root')
 );
