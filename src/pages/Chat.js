49c50d2d159ac0b0b4c3ad02be066903f910d163
Euan_G <euan@amwic.co.za>
2017-03-09 13:25:48 +0200

    Styling finished

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -38,7 +38,7 @@ class Chat extends React.Component {
   render() {
     return (
       <div className="container is-fluid">
-      <br />
+        <br />
         <NewChat chats={this.state.messages} />
         <Container />
       </div>
--- src/pages/components/Container.js
+++ src/pages/components/Container.js
@@ -3,57 +3,57 @@ import Rebase from 're-base';
 import Message from './Message';
 
 const base = Rebase.createClass({
-  apiKey: "AIzaSyDYliJuBFjLAQO_vfH0rlioWUiKrRe_ZL4",
-  authDomain: "tchatapp-586ab.firebaseapp.com",
-  databaseURL: "https://tchatapp-586ab.firebaseio.com",
-  storageBucket: "tchatapp-586ab.appspot.com",
-  messagingSenderId: "846810590536"
+	apiKey: "AIzaSyDYliJuBFjLAQO_vfH0rlioWUiKrRe_ZL4",
+	authDomain: "tchatapp-586ab.firebaseapp.com",
+	databaseURL: "https://tchatapp-586ab.firebaseio.com",
+	storageBucket: "tchatapp-586ab.appspot.com",
+	messagingSenderId: "846810590536"
 });
 
 class Container extends React.Component {
-		constructor(props) {
-			super(props);
-			this.state = {
-				messages : []
-			};
+	constructor(props) {
+		super(props);
+		this.state = {
+			messages: []
 		};
+	};
 
-		componentWillMount() {
-			this.ref = base.syncState('chats', {
-				context: this,
-				state: 'messages',
-				asArray: true
-			})
-		};
+	componentWillMount() {
+		this.ref = base.syncState('chats', {
+			context: this,
+			state: 'messages',
+			asArray: true
+		})
+	};
 
-		componentWillUnmount() {
-			base.removeBinding(this.ref);
-		}
+	componentWillUnmount() {
+		base.removeBinding(this.ref);
+	}
 
-		_removeMessage(index,e) {
-			e.stopPropagation();
-			let arr = this.state.messages.concat([]);
-			arr.splice(index,1);
+	_removeMessage(index, e) {
+		e.stopPropagation();
+		let arr = this.state.messages.concat([]);
+		arr.splice(index, 1);
 
-			this.setState({
-				messages: arr
-			})
-		}
+		this.setState({
+			messages: arr
+		})
+	}
 
-			render() {
-				let messages = this.state.messages.map((item, index) => {
-					return (
-						<Message thread={item} removeMessage={this._removeMessage.bind(this, index)} key={index} className="container"/>
-					);
-				})
+	render() {
+		let messages = this.state.messages.map((item, index) => {
+			return (
+				<Message thread={item} removeMessage={this._removeMessage.bind(this, index)} key={index} className="container" />
+			);
+		})
 
-				return (
-					<div className="">
-						<h1 className="subtitle is-6 has-text-centered">{ (this.state.messages.length || 0) + ' messages' }</h1>
-						<ul>{ messages }</ul>
-					</div>
-				);
-			}
+		return (
+			<div className="">
+				<h1 className="subtitle is-6 has-text-centered">{(this.state.messages.length || 0) + ' messages'}</h1>
+				<ul>{messages}</ul>
+			</div>
+		);
+	}
 }
 
 export default Container;
