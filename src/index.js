f9a3155ec3718fabbfb65338c2f044db78b025b9
Euan_G <euan@amwic.co.za>
2017-03-09 13:47:46 +0200

    refactored folder structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/App';
+import App from './app/App';
 import 'bulma/css/bulma.css';
 import 'font-awesome/css/font-awesome.css';
 import 'firebaseui/dist/firebaseui.css';
