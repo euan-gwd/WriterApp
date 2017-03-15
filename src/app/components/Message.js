4bf4fae125335fbb32a34845e0e1f8e50f0729ef
Euan_G <euan@amwic.co.za>
2017-03-15 14:44:16 +0200

    refactored message styles

--- src/app/components/Message.js
+++ src/app/components/Message.js
@@ -6,7 +6,7 @@ class Message extends React.Component {
 
 	render() {
 		return (
-			<div className="messages-list">
+			<div className="messages-padding">
 				<div className="box">
 					<div className="media">
 						<div className="media-left">
@@ -24,20 +24,26 @@ class Message extends React.Component {
 							</div>
 							{this.props.thread.hasOwnProperty("messageImage") ?
 								<div className="media-content">
-									<div className="content">
-										<img src={this.props.thread.messageImage} alt="messageImage" className="image"/>
-									</div>
+									<img src={this.props.thread.messageImage} alt="messageImage" className="image" />
 								</div> : null}
 							<nav className="level">
 								<div className="level-left">
-									<a className="level-item" type="button">
-										<span className="icon"><i className="fa fa-pencil" aria-hidden="true" ></i></span>
+									<a className="level-item px-2" type="button">
+										<span className="icon is-small"><i className="fa fa-reply" aria-hidden="true" ></i></span>
 									</a>
-									<a onClick={this.props.removeMessage.bind(null)} className='level-item'>
-										<span className="icon"><i className="fa fa-times" aria-hidden="true" ></i></span>
+									<a className="level-item px-2" type="button">
+										<span className="icon is-small"><i className="fa fa-retweet" aria-hidden="true" ></i></span>
+									</a>
+									<a className="level-item px-2" type="button">
+										<span className="icon is-small"><i className="fa fa-heart" aria-hidden="true" ></i></span>
 									</a>
 								</div>
 							</nav>
+						</div>
+						<div className="media-right">
+							<a onClick={this.props.removeMessage.bind(null)}>
+								<span className="icon"><i className="fa fa-times" aria-hidden="true" ></i></span>
+							</a>
 						</div>
 					</div>
 				</div>
