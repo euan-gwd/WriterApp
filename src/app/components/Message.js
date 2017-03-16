245009c0d47c63e34ec9625527c756217c925722
Euan_G <euan@amwic.co.za>
2017-03-16 23:20:39 +0200

    cleaned up initial message box

--- src/app/components/Message.js
+++ src/app/components/Message.js
@@ -15,25 +15,33 @@ class Message extends React.Component {
           </div>
           <div className="media-content">
             <div className="content">
-              <p>
-                <span className="title is-5 pr-1">{this.props.thread.userName}</span>
+              <a onClick={this.props.removeMessage.bind(null)} className="is-pulled-right">
+                <span className="icon is-small">
+                  <i className="fa fa-times" aria-hidden="true"></i>
+                </span>
+              </a>
+              <div>
+                <span className="title is-5 pr">{this.props.thread.userName}</span>
                 <span className="subtitle is-6">{this.props.thread.userEmail}</span>
-              </p>
-              {this.props.thread.message}
+              </div>
+              <div>
+                {this.props.thread.message}
+                {this.props.thread.hasOwnProperty("messageImage")
+                  ? <div className="media-content px-1">
+                      <img src={this.props.thread.messageImage} alt="messageImage" className="image message-image-rounded"/>
+                    </div>
+                  : <div></div>}
+              </div>
+              <div className="">
+                <a className="pr-1">
+                  <i className="fa fa-reply fa-fw" aria-hidden="true"></i>
+                </a>
+                <a className="pr-1">
+                  <i className="fa fa-pencil fa-fw" aria-hidden="true"></i>
+                </a>
+                <small className="has-text-right">{this.props.thread.datetime}</small>
+              </div>
             </div>
-            {this.props.thread.hasOwnProperty("messageImage")
-              ? <div className="media-content">
-                  <img src={this.props.thread.messageImage} alt="messageImage" className="image message-image-rounded"/>
-                </div>
-              : <div></div>}
-            <small>{this.props.thread.datetime}</small>
-          </div>
-          <div className="media-right">
-            <a onClick={this.props.removeMessage.bind(null)}>
-              <span className="icon is-small">
-                <i className="fa fa-times" aria-hidden="true"></i>
-              </span>
-            </a>
           </div>
         </div>
       </div>
