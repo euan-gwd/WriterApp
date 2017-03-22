5943ff98e583c04b387851e5724af0f47c2d927a
Euan_G <euan@amwic.co.za>
2017-03-22 17:31:34 +0200

    refactored edit feature code

--- src/app/components/Scribe.js
+++ src/app/components/Scribe.js
@@ -10,16 +10,12 @@ class Scribe extends React.Component {
     }
   }
 
-  handleEdit() {
-    this.setState({
-      checked: true
-    })
+  handleEditBtnClick() {
+    this.setState({checked: true})
   }
 
-  onChildChanged(newState) {
-    this.setState({
-      checked: newState
-    })
+  onScribeEdited(newState) {
+    this.setState({checked: newState})
   }
 
   render() {
@@ -28,10 +24,10 @@ class Scribe extends React.Component {
         <article className="media">
           <div className="media-left">
             {this.props.thread.hasOwnProperty("userPhoto")
-        ? <figure className="image is-48x48">
+              ? <figure className="image is-48x48">
                   <img src={this.props.thread.userPhoto} alt="profilePic" className="scribe-image-rounded"/>
                 </figure>
-        : <i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>}
+              : <i className="fa fa-user-circle-o fa-2x" aria-hidden="true"></i>}
           </div>
           <div className="media-content">
             <div className="content">
@@ -47,29 +43,28 @@ class Scribe extends React.Component {
               <div>
                 {this.props.thread.scribe}
                 {this.props.thread.hasOwnProperty("scribeImage")
-        ? <div className="media-content px-1">
+                  ? <div className="media-content px-1">
                       <img src={this.props.thread.scribeImage} alt="scribeImage" className="image scribe-image-rounded"/>
                     </div>
-        : <div className="px-1"></div>}
+                  : <div className="px-1"></div>}
               </div>
               <div className="">
                 <a className="pr-1">
                   <i className="fa fa-reply fa-fw" aria-hidden="true"></i>
                 </a>
-                <a className="pr-1" onClick={this.handleEdit.bind(this)}>
+                <a className="pr-1" onClick={this.handleEditBtnClick.bind(this)}>
                   <i className="fa fa-pencil fa-fw" aria-hidden="true"></i>
                 </a>
                 <small className="has-text-right">{this.props.thread.datetime}</small>
               </div>
             </div>
-            { this.state.checked
-        ? <EditScribe currentScribe={this.props.thread} charCount={this.props.thread.scribeCharCount} initialChecked={this.state.checked} callbackParent={(newState) => this.onChildChanged(newState) } />
-        : null
-      }
+            {this.state.checked
+              ? <EditScribe currentScribe={this.props.thread} charCount={this.props.thread.scribeCharCount} initialChecked={this.state.checked} callbackParent={(newState) => this.onScribeEdited(newState)}/>
+              : null}
           </div>
         </article>
       </div>
-      );
+    );
   }
 }
 
