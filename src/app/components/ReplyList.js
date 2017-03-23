37dd89b40fb5866d8bf95dd1922deffacd13747c
Euan_G <euan@amwic.co.za>
2017-03-23 12:31:39 +0200

    Added unique key for replies map

--- src/app/components/ReplyList.js
+++ src/app/components/ReplyList.js
@@ -6,14 +6,15 @@ class ReplyList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      replies: []
+      replies: [],
+						scribeKey: this.props.currentScribe.key
     };
   }
   ;
 
   componentDidMount() {
-    let scribeKey = this.props.currentScribe.key;
-    this.ref = base.bindToState('msgList/' + scribeKey + '/', {
+    let keyRef = this.state.scribeKey;
+    this.ref = base.bindToState('msgList/' + keyRef + '/', {
       context: this,
       state: 'replies',
       asArray: true
@@ -25,14 +26,14 @@ class ReplyList extends React.Component {
     base.removeBinding(this.ref);
   }
 
-  deleteReply(item, evt) {
+  deleteReply(replyItem, evt) {
     evt.stopPropagation();
     let msgListRef = base.database().ref('msgList/');
-    let itemId = item.key;
-    let imgRef = item.replyImage;
-    let replyUID = item.userName;
+    let itemId = replyItem.key;
+    let imgRef = replyItem.replyImage;
+    let replyUID = replyItem.userName;
     let currentUID = this.props.userName;
-    if (item.hasOwnProperty("replyImage")) {
+    if (replyItem.hasOwnProperty("replyImage")) {
       let deleteImgRef = base.storage().refFromURL(imgRef);
       if (replyUID === currentUID) {
         msgListRef.child(itemId).remove(); //removes item from firebase RTdBase
@@ -46,8 +47,8 @@ class ReplyList extends React.Component {
   }
 
   render() {
-    let replies = this.state.replies.map((item) => {
-      return (<Reply thread={item} removeReply={this.deleteReply.bind(this, item)} key={item.key}/>);
+    let replies = this.state.replies.map((replyItem) => {
+      return (<Reply thread={replyItem} removeReply={this.deleteReply.bind(this, replyItem)} key={replyItem.key}/>);
     })
     return (
       <article className="Media">
