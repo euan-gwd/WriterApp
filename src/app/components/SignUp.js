7392ec005ee8750b99066bab922fed663f5cf6d0
Euan_G <euan@amwic.co.za>
2017-04-05 11:50:46 +0200

    Scaffold out signup form

--- src/app/components/SignUp.js
+++ src/app/components/SignUp.js
@@ -1,52 +1,131 @@
 import React from 'react';
+import * as firebase from 'firebase';
 import './scribes.css';
 
 class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signUpUser: this.props.initialState,
+      emailText: '',
+      passText: '',
+      emailErr: 'invisible',
+      passErr: 'invisible'
+    };
+  }
+
+  handleUserSignUp = (evt) => {
+    evt.preventDefault();
+    let email = this.state.emailText.toString();
+    let pass = this.state.passText.toString();
+    let emailValid = false;
+    let passValid = false;
+    if (email.length < 4) {
+      this.setState({emailErr: 'visible'});
+      emailValid = false;
+    } else if (email.length === '') {
+      this.setState({emailErr: 'invisible'});
+      emailValid = false;
+    } else {
+      this.setState({passErr: 'invisible'});
+      emailValid = true;
+    }
+    if (pass.length < 4) {
+      this.setState({passErr: 'visible'});
+      passValid = false;
+    } else if (pass.length === '') {
+      this.setState({passErr: 'invisible'});
+      passValid = false;
+    } else {
+      this.setState({passErr: 'invisible'});
+      passValid = true;
+    }
+    if (emailValid && passValid) {
+      firebase.auth().signInWithEmailAndPassword(email, pass).catch(err => {
+        console.log(err);
+      });
+    }
+
+  }
+
+  handleCancel = (evt) => {
+    const newState = !this.state.signUpUser;
+    this.props.callbackParent(newState);
+  }
+
+  handleEmailInput = (evt) => {
+    this.setState({emailText: evt.target.value});
+  }
+
+		handleUserNameInput = (evt) => {
+    this.setState({userNameText: evt.target.value});
+  }
+
+  handlePassInput = (evt) => {
+    this.setState({passText: evt.target.value});
+  }
+
+  componentWillUnmount() {
+    this.setState({emailText: '', passText: ''});
+  }
 
   render() {
     return (
-      <div className="backing">
-        <div className="login-container">
-          <div className="card box">
-            <header className="card-header">
-              <p className="card-header-title">Sign Up</p>
-            </header>
-            <form className="card-content">
+      <div className="modal-background">
+        <div className="login-container is-overlay">
+          <div className="signIn-card">
+            <form className="modal-card-body" onSubmit={this.handleUserSignUp.bind(this)}>
               <div className="field">
                 <p className="control has-icon">
-                  <input className="input" type="text" placeholder="Full Name"/>
-                  <span className="icon is-small">
-                    <i className="fa fa-address-card"></i>
-                  </span>
-                </p>
-              </div>
-              <div className="field">
-                <p className="control has-icon">
-                  <input className="input" type="email" placeholder="Email"/>
+                  <input className="input" defaultValue={this.state.userNameText} type="text" placeholder="UserName" onChange={this.handleEmailInput.bind(this)}/>
                   <span className="icon is-small">
                     <i className="fa fa-envelope"></i>
                   </span>
                 </p>
               </div>
-              <div className="field">
-                <p className="control has-icon">
-                  <input className="input" type="password" placeholder="Password"/>
-                  <span className="icon is-small">
-                    <i className="fa fa-lock"></i>
-                  </span>
-                </p>
-              </div>
-              <div className="field">
-                <p className="control has-icon">
-                  <input className="input" type="password" placeholder="Verify Password"/>
-                  <span className="icon is-small">
-                    <i className="fa fa-lock"></i>
-                  </span>
-                </p>
-              </div>
-              <div className="field">
+
+              {(this.state.emailErr === 'visible')
+                ? <div className="field">
+                    <p className="control has-icon has-icon-right">
+                      <input className="input is-danger" defaultValue={this.state.emailText} type="email" placeholder="Email" onChange={this.handleEmailInput.bind(this)}/>
+                      <span className="icon is-small">
+                        <i className="fa fa-warning"></i>
+                      </span>
+                    </p>
+                    <span className="help is-danger">Please enter a valid email address.</span>
+                  </div>
+                : <div className="field">
+                  <p className="control has-icon">
+                    <input className="input" defaultValue={this.state.emailText} type="email" placeholder="Email" onChange={this.handleEmailInput.bind(this)}/>
+                    <span className="icon is-small">
+                      <i className="fa fa-envelope"></i>
+                    </span>
+                  </p>
+                </div>}
+              {(this.state.passErr === 'visible')
+                ? <div className="field">
+                    <p className="control has-icon has-icon-right">
+                      <input className="input is-danger" defaultValue={this.state.passText} type="password" placeholder="Password" onChange={this.handlePassInput.bind(this)}/>
+                      <span className="icon is-small">
+                        <i className="fa fa-warning"></i>
+                      </span>
+                    </p>
+                    <span className="help is-danger">Please enter a valid password.</span>
+                  </div>
+                : <div className="field">
+                  <p className="control has-icon">
+                    <input className="input" defaultValue={this.state.passText} type="password" placeholder="Password" onChange={this.handlePassInput.bind(this)}/>
+                    <span className="icon is-small">
+                      <i className="fa fa-lock"></i>
+                    </span>
+                  </p>
+                </div>}
+              <div className="field is-group">
                 <p className="control">
                   <button type="submit" className="button is-success is-outlined">Sign Up</button>
+                </p>
+                <p className="control">
+                  <button onClick={this.handleCancel} className="button is-light is-outlined">Cancel</button>
                 </p>
               </div>
             </form>
