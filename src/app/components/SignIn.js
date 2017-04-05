c44389d2dde01575a574118431e326c065c86a24
Euan_G <euan@amwic.co.za>
2017-04-05 09:57:57 +0200

    Added validation styles to User sign In

--- src/app/components/SignIn.js
+++ src/app/components/SignIn.js
@@ -1,36 +1,91 @@
 import React from 'react';
+import * as firebase from 'firebase';
 import './scribes.css';
 
 class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailText: '',
+      passText: '',
+      emailErr: 'invisible',
+      passErr: 'invisible',
+      signInUser: this.props.initialState,
+    };
+  }
+
+  handleUserSignIn = (evt) => {
+    evt.preventDefault();
+    let email = this.state.emailText;
+    let pass = this.state.passText;
+    let emailValid = false;
+    let passValid = false;
+    if (email.length < 4) {
+      this.setState({ emailErr: 'visible' });
+      emailValid = false;
+    } else {
+      emailValid = true;
+    }
+    if (pass.length < 4) {
+      this.setState({ passErr: 'visible' });
+      passValid = false;
+    } else {
+      passValid = true;
+    }
+    if (emailValid && passValid) {
+      firebase.auth().signInWithEmailAndPassword(email, pass).catch(err => {
+        console.log(err);
+      });
+    }
+  }
+
+  handleCancel = (evt) => {
+    const newState = !this.state.signInUser;
+    this.props.callbackParent(newState);
+  }
+
+  handleEmailInput = (evt) => {
+    this.setState({ emailText: evt.target.value });
+  }
+
+  handlePassInput = (evt) => {
+    this.setState({ passText: evt.target.value });
+  }
+
+  componentWillUnmount() {
+    this.setState({ emailText: '', passText: '' });
+  }
 
   render() {
     return (
-      <div className="backing">
-        <div className="login-container">
+      <div className="modal-background">
+        <div className="login-container is-overlay">
           <div className="card box">
-            <header className="card-header">
-              <p className="card-header-title">Sign In</p>
-            </header>
-            <form className="card-content">
+            <form className="card-content" onSubmit={this.handleUserSignIn.bind(this)}>
               <div className="field">
                 <p className="control has-icon">
-                  <input className="input" type="email" placeholder="Email"/>
+                  <input className="input" defaultValue={this.state.emailText} type="email" placeholder="Email" onChange={this.handleEmailInput.bind(this)} />
                   <span className="icon is-small">
                     <i className="fa fa-envelope"></i>
                   </span>
                 </p>
+                <span className={`help is-danger ${this.state.emailErr}`}>Please enter a valid email address.</span>
               </div>
               <div className="field">
                 <p className="control has-icon">
-                  <input className="input" type="password" placeholder="Password"/>
+                  <input className="input" defaultValue={this.state.passText} type="password" placeholder="Password" onChange={this.handlePassInput.bind(this)} />
                   <span className="icon is-small">
                     <i className="fa fa-lock"></i>
                   </span>
                 </p>
+                <span className={`help is-danger ${this.state.passErr}`}>Please enter a valid password.</span>
               </div>
-              <div className="field">
+              <div className="field is-group">
                 <p className="control">
                   <button type="submit" className="button is-success is-outlined">Sign In</button>
+                </p>
+                <p className="control">
+                  <button onClick={this.handleCancel} className="button is-light is-outlined">Cancel</button>
                 </p>
               </div>
             </form>
--- src/app/components/SignedOut.js
+++ src/app/components/SignedOut.js
@@ -1,65 +1,82 @@
 import React from 'react';
-import * as firebase from 'firebase';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+// import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import logo from '../logo.svg';
 import './scribes.css';
 
 class SignedOut extends React.Component {
-
-  handleSignedInUser = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInUser: false,
+      signUpUser: false
+    }
   }
 
-  handleUserSignUp = () => {}
+  handleSignInBtnClick() {
+    this.setState({signInUser: true})
+  }
+
+  onUserSignIn(newState) {
+    this.setState({signInUser: newState})
+  }
+
+  handleSignUpBtnClick() {
+    this.setState({signUpUser: true})
+  }
+
+  onUserSignUp(newState) {
+    this.setState({signUpUser: newState})
+  }
 
   render() {
     return (
-      <Router>
-        <div className="">
-          <nav className="nav has-shadow">
-            <div className="nav-center">
-              <div className="nav-item">
-                <img src={logo} alt="logo" className="App-logo"/>
-                <h1 className="title is-hidden-mobile">Village Scriber</h1>
-              </div>
-														<div className="nav-item"></div>
-              <div className="nav-item">
-                <Link to="/SignIn" id="sign-in" className="button is-success is-outlined is-small">
-                  <span className="icon is-small">
-                    <i className="fa fa-sign-in"></i>
-                  </span>
-                  <span className="is-hidden-mobile">Log In</span>
-                </Link>
-              </div>
-              <div className="nav-item">
-                <Link to="/SignUp" id="sign-in" className="button is-info is-outlined is-small">
-                  <span className="icon is-small">
-                    <i className="fa fa-user-plus"></i>
-                  </span>
-                  <span className="is-hidden-mobile">Sign Up</span>
-                </Link>
-              </div>
+      <div>
+        <nav className="nav has-shadow">
+          <div className="nav-center">
+            <div className="nav-item">
+              <img src={logo} alt="logo" className="App-logo"/>
+              <h1 className="title is-hidden-mobile">Village Scriber</h1>
             </div>
-          </nav>
-          <section className="hero is-light is-small is-bold">
-            <div className="hero-body">
-              <div className="container">
-                <h1 className="title has-text-centered">
-                  What's a happening...?
-                </h1>
-                <h2 className="subtitle has-text-centered">
-                  Join the conversation...
-                </h2>
-              </div>
+            <div className="nav-item"></div>
+            <div className="nav-item">
+              <a id="sign-in" className="button is-success is-outlined is-small" onClick={this.handleSignInBtnClick.bind(this)}>
+                <span className="icon is-small">
+                  <i className="fa fa-sign-in"></i>
+                </span>
+                <span className="is-hidden-mobile">Log In</span>
+              </a>
             </div>
-          </section>
-          <Route path="/SignIn" component={SignIn} className="backing"/>
-          <Route path="/SignUp" component={SignUp} className="backing"/>
-        </div>
-      </Router>
+            <div className="nav-item">
+              <a id="sign-in" className="button is-info is-outlined is-small" onClick={this.handleSignUpBtnClick.bind(this)}>
+                <span className="icon is-small">
+                  <i className="fa fa-user-plus"></i>
+                </span>
+                <span className="is-hidden-mobile">Sign Up</span>
+              </a>
+            </div>
+          </div>
+        </nav>
+        <section className="hero is-light is-large is-bold">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title has-text-centered">
+                What's a happening...?
+              </h1>
+              <h2 className="subtitle has-text-centered">
+                Join the conversation...
+              </h2>
+            </div>
+          </div>
+        </section>
+        {this.state.signInUser
+          ? <SignIn initialState={this.state.signInUser} callbackParent={(newState) => this.onUserSignIn(newState)}/>
+          : null}
+        {this.state.signUpUser
+          ? <SignUp initialState={this.state.signUpUser} callbackParent={(newState) => this.onUserSignUp(newState)}/>
+          : null}
+      </div>
     );
   }
 
