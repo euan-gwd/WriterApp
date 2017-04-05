40d3e33e92f1721fb4ab2624270ba4cd8dfb3376
Euan_G <euan@amwic.co.za>
2017-04-05 11:18:07 +0200

    Added form validation

--- src/app/components/SignIn.js
+++ src/app/components/SignIn.js
@@ -6,11 +6,11 @@ class SignIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      signInUser: this.props.initialState,
       emailText: '',
       passText: '',
       emailErr: 'invisible',
       passErr: 'invisible',
-      signInUser: this.props.initialState,
     };
   }
 
@@ -23,13 +23,21 @@ class SignIn extends React.Component {
     if (email.length < 4) {
       this.setState({ emailErr: 'visible' });
       emailValid = false;
+    } else if (email.length === '') {
+      this.setState({ emailErr: 'invisible' });
+      emailValid = false;
     } else {
+      this.setState({ passErr: 'invisible' });
       emailValid = true;
     }
     if (pass.length < 4) {
       this.setState({ passErr: 'visible' });
       passValid = false;
+    } else if (pass.length === '') {
+      this.setState({ passErr: 'invisible' });
+      passValid = false;
     } else {
+      this.setState({ passErr: 'invisible' });
       passValid = true;
     }
     if (emailValid && passValid) {
@@ -37,6 +45,7 @@ class SignIn extends React.Component {
         console.log(err);
       });
     }
+
   }
 
   handleCancel = (evt) => {
@@ -60,26 +69,44 @@ class SignIn extends React.Component {
     return (
       <div className="modal-background">
         <div className="login-container is-overlay">
-          <div className="card box">
-            <form className="card-content" onSubmit={this.handleUserSignIn.bind(this)}>
-              <div className="field">
-                <p className="control has-icon">
-                  <input className="input" defaultValue={this.state.emailText} type="email" placeholder="Email" onChange={this.handleEmailInput.bind(this)} />
-                  <span className="icon is-small">
-                    <i className="fa fa-envelope"></i>
-                  </span>
-                </p>
-                <span className={`help is-danger ${this.state.emailErr}`}>Please enter a valid email address.</span>
-              </div>
-              <div className="field">
-                <p className="control has-icon">
-                  <input className="input" defaultValue={this.state.passText} type="password" placeholder="Password" onChange={this.handlePassInput.bind(this)} />
-                  <span className="icon is-small">
-                    <i className="fa fa-lock"></i>
-                  </span>
-                </p>
-                <span className={`help is-danger ${this.state.passErr}`}>Please enter a valid password.</span>
-              </div>
+          <div className="signIn-card">
+            <form className="modal-card-body" onSubmit={this.handleUserSignIn.bind(this)}>
+              {(this.state.emailErr === 'visible')
+                ? <div className="field">
+                  <p className="control has-icon has-icon-right">
+                    <input className="input is-danger" defaultValue={this.state.emailText} type="email" placeholder="Email" onChange={this.handleEmailInput.bind(this)} />
+                    <span className="icon is-small">
+                      <i className="fa fa-warning"></i>
+                    </span>
+                  </p>
+                  <span className="help is-danger">Please enter a valid email address.</span>
+                </div>
+                : <div className="field">
+                  <p className="control has-icon">
+                    <input className="input" defaultValue={this.state.emailText} type="email" placeholder="Email" onChange={this.handleEmailInput.bind(this)} />
+                    <span className="icon is-small">
+                      <i className="fa fa-envelope"></i>
+                    </span>
+                  </p>
+                </div>}
+              {(this.state.passErr === 'visible')
+                ? <div className="field">
+                  <p className="control has-icon has-icon-right">
+                    <input className="input is-danger" defaultValue={this.state.passText} type="password" placeholder="Password" onChange={this.handlePassInput.bind(this)} />
+                    <span className="icon is-small">
+                      <i className="fa fa-warning"></i>
+                    </span>
+                  </p>
+                  <span className="help is-danger">Please enter a valid password.</span>
+                </div>
+                : <div className="field">
+                  <p className="control has-icon">
+                    <input className="input" defaultValue={this.state.passText} type="password" placeholder="Password" onChange={this.handlePassInput.bind(this)} />
+                    <span className="icon is-small">
+                      <i className="fa fa-lock"></i>
+                    </span>
+                  </p>
+                </div>}
               <div className="field is-group">
                 <p className="control">
                   <button type="submit" className="button is-success is-outlined">Sign In</button>
