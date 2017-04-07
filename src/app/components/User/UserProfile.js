a38c622798fc1c5eb3c6df04a756beb530d79bab
Euan_G <euan@amwic.co.za>
2017-04-07 15:04:45 +0200

    User Profile Scaffolding done

--- src/app/components/User/UserProfile.js
+++ src/app/components/User/UserProfile.js
@@ -2,32 +2,46 @@ import React from 'react';
 import * as firebase from "firebase";
 import AddScribe from './AddScribe';
 import Scribe from './Scribe';
-import defaultUserPic from '../Default_User_Pic.svg';
+import defaultUserPic from '../../Default_User_Pic.svg';
 
-class ScribeList extends React.Component {
+class UserProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       scribes: [],
-      starred: false
+      starred: false,
+      userId: null,
+      userName: null,
+      userEmail: null,
+      userPhoto: null
     };
   };
 
   componentDidMount() {
+    let user = firebase.auth().currentUser;
+    if (user != null) {
+      this.setState({
+        userId: user.uid,
+        userName: user.displayName,
+        userEmail: user.email,
+        userPhoto: user.photoURL
+      })
+    }
+    
     firebase.database().ref('mainTL').on('value', (res) => {
-      const userData = res.val();
-      const dataArray = [];
-      for (let objKey in userData) {
-        userData[objKey].key = objKey;
-        dataArray.push(userData[objKey])
+      const scribeData = res.val();
+      const scribeDataArray = [];
+      for (let objKey in scribeData) {
+        scribeData[objKey].key = objKey;
+        scribeDataArray.push(scribeData[objKey])
       }
-      this.setState({ scribes: dataArray })
-    })
+      this.setState({ scribes: scribeDataArray })
+    });
   };
 
   deleteScribe(item, evt) {
     evt.stopPropagation();
-    let userId = this.props.userId;
+    let userId = this.state.userId;
     let mainTLRef = firebase.database().ref('mainTL/');
     let userTLRef = firebase.database().ref('userTL/' + userId + '/');
     if (item.hasOwnProperty("scribeImage")) {
@@ -59,7 +73,7 @@ class ScribeList extends React.Component {
 
   toggleLikes(item, evt) {
     evt.stopPropagation();
-    let userId = this.props.userId;
+    let userId = this.state.userId;
     let mainDbRef = firebase.database().ref('mainTL/').child(item.key).child('likes');
     let userDbRef = firebase.database().ref('userTL/' + userId + '/').child(item.key).child('likes');
     (this.state.starred === true)
@@ -84,17 +98,17 @@ class ScribeList extends React.Component {
               <div className="card-content">
                 <div className="media">
                   <div className="media-left">
-                    {(this.props.userPhoto === null)
+                    {(this.state.userPhoto === null)
                       ? <figure className="image is-48x48 is-border-image">
                         <img src={defaultUserPic} alt="defaultProfilePic" className="image-rounded" />
                       </figure>
                       : <figure className="image is-48x48 is-border-image">
-                        <img src={this.props.userPhoto} alt="profilePic" className="image-rounded" />
+                        <img src={this.state.userPhoto} alt="profilePic" className="image-rounded" />
                       </figure>}
                   </div>
                   <div className="media-content">
-                    <p className="title is-5 pr">{this.props.userName}</p>
-                    <p className="subtitle is-6 lh-1">{this.props.userEmail}</p>
+                    <p className="title is-5 pr">{this.state.userName}</p>
+                    <p className="subtitle is-6 lh-1">{this.state.userEmail}</p>
                   </div>
                 </div>
                 <footer className="leveled">
@@ -121,7 +135,7 @@ class ScribeList extends React.Component {
             </div>
           </div>
           <div className="column is-7">
-            <AddScribe mainTL={this.state.scribes} userName={this.props.userName} userId={this.props.userId} userEmail={this.props.userEmail} userPhoto={this.props.userPhoto} />
+            <AddScribe mainTL={this.state.scribes} userName={this.state.userName} userId={this.state.userId} userEmail={this.state.userEmail} userPhoto={this.state.userPhoto} />
             <ul className="">{scribes}</ul>
           </div>
         </div>
@@ -130,4 +144,4 @@ class ScribeList extends React.Component {
   }
 }
 
-export default ScribeList;
+export default UserProfile;
