6d8431ce3a7b717f900f4f49b441e2ecb9ffef4b
Euan_G <euan@amwic.co.za>
2017-04-10 14:42:40 +0200

    Added routing without react Router

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import * as firebase from 'firebase';
 import config from './firebase.config';
+import {Switch, Case, Default} from 'jsx-switch';
 import ScribeList from './components/ScribeList';
 import SignedOut from './components/SignedOut';
+import Messages from './components/Messages';
+import UserProfile from './components/UserProfile';
 import logo from './logo.svg';
 import defaultUserPic from './Default_User_Pic.svg';
 import './App.css';
@@ -14,7 +17,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      auth: false
+      auth: false,
+      route: 'home'
     }
   }
 
@@ -22,20 +26,36 @@ class App extends React.Component {
     firebase.auth().signOut();
   }
 
+  handleHomeRoute = (evt) => {
+    this.setState({route: 'Home'})
+  }
+
+  handleMessagesRoute = (evt) => {
+    this.setState({route: 'Messages'})
+  }
+
+  handleUserProfileRoute = (evt) => {
+    this.setState({route: 'UserProfile'})
+  }
+
   componentDidMount() {
-    firebase.auth().onAuthStateChanged((user) => {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        this.setState({ auth: user });
+        this.setState({auth: user});
         this.registerUser(user);
       } else {
-        this.setState({ auth: false });
+        this.setState({auth: false});
       }
     });
   }
 
   registerUser(user) {
     const userRef = firebase.database().ref('users/' + user.uid);
-    userRef.update({ name: user.displayName, email: user.email, photoUrl: user.photoURL });
+    userRef.update({name: user.displayName, email: user.email, photoUrl: user.photoURL});
+  }
+
+  componentWillUnmount() {
+    this.removeListener()
   }
 
   render() {
@@ -43,38 +63,52 @@ class App extends React.Component {
       <div>
         {(this.state.auth)
           ? <div className="">
-            <nav className="nav has-shadow">
-              <div className="grid-container">
-                <a className="nav-item is-tab" data-balloon="Home" data-balloon-pos="down">
-                  {/* <span className="icon"> */}
+              <nav className="nav has-shadow">
+                <div className="grid-container">
+                  <a className="nav-item is-tab" data-balloon="Home" data-balloon-pos="down" onClick={this.handleHomeRoute}>
+                    {/* <span className="icon"> */}
                     <i className="fa fa-home fa-2x"></i>
-                  {/* </span> */}
-                </a>
-                <a className="nav-item is-tab" data-balloon="Messages" data-balloon-pos="down">
-                  {/* <span className="icon"> */}
+                    {/* </span> */}
+                  </a>
+                  <a className="nav-item is-tab" data-balloon="Messages" data-balloon-pos="down" onClick={this.handleMessagesRoute}>
+                    {/* <span className="icon"> */}
                     <i className="fa fa-comments-o fa-2x"></i>
-                  {/* </span> */}
-                </a>
-                <div className="nav-item">
-                  <img src={logo} alt="logo" className="App-logo" />
-                  <h1 className="title is-hidden-mobile">Village Scriber</h1>
-                </div>
-                <a className="nav-item is-tab" data-balloon="View Profile" data-balloon-pos="down">
-                  {(this.state.auth.photoURL === null)
-                    ? <img src={defaultUserPic} alt="defaultProfilePic" className="image nav-spacing nav-image-is-rounded" />
-                    : <img src={this.state.auth.photoURL} alt="profilePic" className="image nav-spacing nav-image-is-rounded" />}
-                </a>
-                <a id="sign-out" className="nav-item is-tab" onClick={this.handleSignedOutUser} data-balloon="Sign Out" data-balloon-pos="down">
-                  {/* <span className="icon"> */}
+                    {/* </span> */}
+                  </a>
+                  <div className="nav-item">
+                    <img src={logo} alt="logo" className="App-logo"/>
+                    <h1 className="title is-hidden-mobile">Village Scriber</h1>
+                  </div>
+                  <a className="nav-item is-tab" data-balloon="View Profile" data-balloon-pos="down" onClick={this.handleUserProfileRoute}>
+                    {(this.state.auth.photoURL === null)
+                      ? <img src={defaultUserPic} alt="defaultProfilePic" className="image nav-spacing nav-image-is-rounded"/>
+                      : <img src={this.state.auth.photoURL} alt="profilePic" className="image nav-spacing nav-image-is-rounded"/>}
+                  </a>
+                  <a id="sign-out" className="nav-item is-tab" onClick={this.handleSignedOutUser} data-balloon="Sign Out" data-balloon-pos="down">
+                    {/* <span className="icon"> */}
                     <i className="fa fa-sign-out fa-2x"></i>
-                  {/* </span> */}
-                </a>
-              </div>
-            </nav>
-            <ScribeList />
-          </div>
+                    {/* </span> */}
+                  </a>
+                </div>
+              </nav>
+              <Switch>
+                <Case expr={this.state.route === 'Home'}>
+                  <ScribeList/>
+                </Case>
+                <Case expr={this.state.route === 'Messages'}>
+                  <Messages/>
+                </Case>
+                <Case expr={this.state.route === 'UserProfile'}>
+                  <UserProfile/>
+                </Case>
+                <Default>
+                  <ScribeList/>
+                </Default>
+              </Switch>
+              {/* <ScribeList/> */}
+            </div>
           : <div className="">
-            <SignedOut />
+            <SignedOut/>
           </div>}
       </div>
     );
---
+++ src/app/components/UserProfile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class UserProfile extends React.Component {
+
+	render() {
+		return (
+			<div>User Profile Tab</div>
+		);
+	}
+
+}
+
+export default UserProfile;
