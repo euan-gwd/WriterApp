a0b74023aab8c95f31f1808146c4f70209f78657
Euan_G <euan@amwic.co.za>
2017-04-19 11:01:42 +0200

    don't show currentUser in follow list

--- src/app/components/Follow.js
+++ src/app/components/Follow.js
@@ -4,47 +4,52 @@ import "./layout.css";
 import './colors.css';
 
 class Follow extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userId: this.props.UserID,
-      userName: null,
-      userPhoto: null
-    };
-  };
+	constructor(props) {
+		super(props);
+		this.state = {
+			userId: this.props.UserID,
+			userName: null,
+			userPhoto: null
+		};
+	};
 
-  componentDidMount() {
-    let userId = this.props.UserID;
-    firebase.database().ref('users/' + userId + '/').once('value', (res) => {
-      const followerData = res.val();
-      const {name, photoUrl} = followerData;
-      this.setState({userName: name, userPhoto: photoUrl});
-    });
-  }
+	componentDidMount() {
+		let userId = this.props.UserID;
+		firebase.database().ref('users/' + userId + '/').once('value', (res) => {
+			const followerData = res.val();
+			const {name, photoUrl} = followerData;
+			this.setState({userName: name, userPhoto: photoUrl});
+		});
+	}
 
-  render() {
-    return (
-      <div className="selected-scribe mb">
-        <div className="media">
-          <div className="media-left">
-            <figure className="image is-32x32">
-              <img src={this.state.userPhoto} alt="profilePic" className="image-rounded"/>
-            </figure>
-          </div>
-          <div className="media-content">
-            <p className="subtitle is-6">{this.state.userName}</p>
-          </div>
-          <div className="media-right">
-            <a className="follow" data-balloon="Follow Writer" data-balloon-pos="left" onClick={this.props.followUser.bind(null)}>
-              <span className="icon is-medium">
-                <i className="fa fa-user-plus" aria-hidden="true"></i>
-              </span>
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		let currentUser = firebase.auth().currentUser.uid;
+		return (
+			<div>
+				{(this.state.userId !== currentUser)
+					? <div className="selected-scribe mb">
+							<div className="media">
+								<div className="media-left">
+									<figure className="image is-32x32">
+										<img src={this.state.userPhoto} alt="profilePic" className="image-rounded"/>
+									</figure>
+								</div>
+								<div className="media-content">
+									<p className="subtitle is-6">{this.state.userName}</p>
+								</div>
+								<div className="media-right">
+									<a className="follow" data-balloon="Follow Writer" data-balloon-pos="left" onClick={this.props.followUser.bind(null)}>
+										<span className="icon is-medium">
+											<i className="fa fa-user-plus" aria-hidden="true"></i>
+										</span>
+									</a>
+								</div>
+							</div>
+						</div>
+					: null}
+			</div>
+		);
+	}
 
 }
 
