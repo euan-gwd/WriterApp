5c6c1b684fd75e60b4d0ae3bcbb95594af0108c3
Euan_G <euan@amwic.co.za>
2017-04-29 12:41:01 +0200

    All is done, finally working correctly

--- src/app/components/Follow.js
+++ src/app/components/Follow.js
@@ -10,7 +10,8 @@ class Follow extends React.Component {
 		this.state = {
 			userId: null,
 			userName: null,
-			userPhoto: null
+			userPhoto: null,
+			following: null
 		};
 	}; //end constructor
 
@@ -91,25 +92,26 @@ class Follow extends React.Component {
 	}; // end toggleFollow
 
 	render() {
+		let currentUser = firebase.auth().currentUser.uid;
 		return (
 			<div>
-				<div className="selected-scribe mb">
-					<div className="media">
-						<div className="media-left">
-							{(this.state.userPhoto === null)
-								? <figure className="image is-32x32">
-										<img src={defaultUserPic} alt="defaultProfilePic" className="image-rounded"/>
-									</figure>
-								: <figure className="image is-32x32">
-									<img src={this.state.userPhoto} alt="profilePic" className="image-rounded"/>
-								</figure>}
-						</div>
-						<div className="media-content">
-							<p className="subtitle is-6">{this.state.userName}</p>
-						</div>
-						<div className="media-right">
-							{(this.state.following !== undefined)
-								? <a className={this.state.following} data-balloon={this.state.following} data-balloon-pos="left" onClick={this.followWriter.bind(this)}>
+				{(this.state.userId !== currentUser)
+					? <div className="selected-scribe mb">
+							<div className="media">
+								<div className="media-left">
+									{(this.state.userPhoto === null)
+										? <figure className="image is-32x32">
+												<img src={defaultUserPic} alt="defaultProfilePic" className="image-rounded"/>
+											</figure>
+										: <figure className="image is-32x32">
+											<img src={this.state.userPhoto} alt="profilePic" className="image-rounded"/>
+										</figure>}
+								</div>
+								<div className="media-content">
+									<p className="subtitle is-6">{this.state.userName}</p>
+								</div>
+								<div className="media-right">
+									<a className={this.state.following} data-balloon={this.state.following} data-balloon-pos="left" onClick={this.followWriter.bind(this)}>
 										{(this.state.following === "follow")
 											? <span className="icon is-medium">
 													<i className="fa fa-user-plus" aria-hidden="true"></i>
@@ -118,14 +120,10 @@ class Follow extends React.Component {
 												<i className="fa fa-user-times" aria-hidden="true"></i>
 											</span>}
 									</a>
-								: <a className="follow" data-balloon="follow" data-balloon-pos="left" onClick={this.followWriter.bind(this)}>
-									<span className="icon is-medium">
-										<i className="fa fa-user-plus" aria-hidden="true"></i>
-									</span>
-								</a>}
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
+					: null}
 			</div>
 		);
 	} //end render
