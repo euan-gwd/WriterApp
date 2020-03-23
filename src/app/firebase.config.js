daec7ac161d1e9a15f461ba322f49f1e498926fa
Euan Greenwood <euan@euangreenwood.com>
2020-03-23 01:47:25 +0000

    Update firebase.config.js

--- src/app/firebase.config.js
+++ src/app/firebase.config.js
@@ -1,12 +1,13 @@
 const config = {
-  apiKey: '',
-  authDomain: 'writersapp-react.firebaseapp.com',
-  databaseURL: 'https://writersapp-react.firebaseio.com',
-  projectId: 'writersapp-react',
-  storageBucket: 'writersapp-react.appspot.com',
-  messagingSenderId: '612530666183',
-  appId: '1:612530666183:web:ab708a188accda13295ce2',
-  measurementId: 'G-BGJ2T4JXE9'
-}
+  apiKey: "AIzaSyATHUCfAr1OT8LwtUarJyLDxXbLf2UymIU",
+  authDomain: "writersapp-react.firebaseapp.com",
+  databaseURL: "https://writersapp-react.firebaseio.com",
+  projectId: "writersapp-react",
+  storageBucket: "writersapp-react.appspot.com",
+  messagingSenderId: "612530666183",
+  appId: "1:612530666183:web:ab708a188accda13295ce2",
+  measurementId: "G-BGJ2T4JXE9"
+};
+
 
 export default config
